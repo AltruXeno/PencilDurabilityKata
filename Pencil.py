eb92f6f46dfb89952a0e165ea7b0aff79aa6061a
AltruPC/Andy <AltruXeno@gmail.com>
2018-04-05 19:51:23 -0400

    *Added test for pencil point durability degradation

--- Pencil.py
+++ Pencil.py
@@ -1,5 +1,10 @@
 
 class Pencil(object):
+    def __init__(self):
+        self.point_durability = 1000
 
     def write(self, paper, string_to_write):
-        paper.text += string_to_write
+
+        for letter in string_to_write:
+            paper.text += letter
+            self.point_durability -= 1
--- PencilDurabilityTester.py
+++ PencilDurabilityTester.py
@@ -5,27 +5,37 @@ from Paper import Paper
 
 class TestPencilDurability(unittest.TestCase):
 
+    # Setup initial object creation
+    def setUp(self):
+        self.pencil = Pencil()
+        self.paper = Paper()
+
     # Tests if single string is correctly written to piece of paper
     def test_write_one_string(self):
-        pencil = Pencil()
-        paper = Paper()
         string_to_write = "which"
 
-        pencil.write(paper, string_to_write)
+        self.pencil.write(self.paper, string_to_write)
 
-        self.assertEqual(paper.text, string_to_write)
+        self.assertEqual(self.paper.text, string_to_write)
 
     # Test is multiple strings are correctly written to piece of paper
     def test_write_multiple_strings(self):
-        pencil = Pencil()
-        paper = Paper()
         string_to_write_1 = "which "
         string_to_write_2 = "wrist"
 
-        pencil.write(paper, string_to_write_1)
-        pencil.write(paper, string_to_write_2)
+        self.pencil.write(self.paper, string_to_write_1)
+        self.pencil.write(self.paper, string_to_write_2)
 
-        self.assertEqual(paper.text, string_to_write_1 + string_to_write_2)
+        self.assertEqual(self.paper.text, string_to_write_1 + string_to_write_2)
+
+    # Test to determine if pencil point durability degrades when writing
+    def test_pencil_point_durability_degradation(self):
+        string_to_write = "which"
+        point_durability = self.pencil.point_durability
+
+        self.pencil.write(self.paper, string_to_write)
+
+        self.assertEqual(self.pencil.point_durability, point_durability - len(string_to_write))
 
 
 if __name__ == "__main__":
