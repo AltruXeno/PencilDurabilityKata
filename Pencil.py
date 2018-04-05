1193bed3a7e29a0e8aa75c3580df02a662063e11
AltruPC/Andy <AltruXeno@gmail.com>
2018-04-05 19:41:01 -0400

    *Added test for multiple strings

--- Pencil.py
+++ Pencil.py
@@ -2,4 +2,4 @@
 class Pencil(object):
 
     def write(self, paper, string_to_write):
-        paper.text = string_to_write
+        paper.text += string_to_write
--- PencilDurabilityTester.py
+++ PencilDurabilityTester.py
@@ -5,7 +5,7 @@ from Paper import Paper
 
 class TestPencilDurability(unittest.TestCase):
 
-    # Tests if singular string is correctly written to piece of paper
+    # Tests if single string is correctly written to piece of paper
     def test_write_one_string(self):
         pencil = Pencil()
         paper = Paper()
@@ -14,6 +14,18 @@ class TestPencilDurability(unittest.TestCase):
         pencil.write(paper, string_to_write)
 
         self.assertEqual(paper.text, string_to_write)
+
+    # Test is multiple strings are correctly written to piece of paper
+    def test_write_multiple_strings(self):
+        pencil = Pencil()
+        paper = Paper()
+        string_to_write_1 = "which "
+        string_to_write_2 = "wrist"
+
+        pencil.write(paper, string_to_write_1)
+        pencil.write(paper, string_to_write_2)
+
+        self.assertEqual(paper.text, string_to_write_1 + string_to_write_2)
 
 
 if __name__ == "__main__":
