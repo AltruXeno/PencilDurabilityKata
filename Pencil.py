d04700d93a65b394d7c71fa07788506600c81261
AltruPC/Andy <AltruXeno@gmail.com>
2018-04-05 21:19:12 -0400

    *Added test to ensure that pencils cannot write with zero point durability *Added start_point_durability to pencil class for easier testing

--- Pencil.py
+++ Pencil.py
@@ -1,11 +1,17 @@
 
 class Pencil(object):
-    def __init__(self):
-        self.point_durability = 1000
+    def __init__(self, point_durability=100):
+        self.start_point_durability = point_durability
+        self.point_durability = point_durability
 
     def write(self, paper, string_to_write):
 
         for letter in string_to_write:
+            # If we have no point durability left, simply write a space
+            if self.point_durability <= 0:
+                paper.text += ' '
+                continue
+
             paper.text += letter
 
             # Don't degrade the pencil if the letter is a specified whitespace
--- PencilDurabilityTester.py
+++ PencilDurabilityTester.py
@@ -1,4 +1,6 @@
 import unittest
+import random
+import string
 from Pencil import Pencil
 from Paper import Paper
 
@@ -65,6 +67,7 @@ class TestPencilDurability(unittest.TestCase):
 
         self.assertEqual(self.pencil.point_durability, point_durability - expected_score)
 
+    # Test to see if uppercase letters cost 2 point durability and lowercase letters cost 1
     def test_pencil_point_durability_degradation_character_case(self):
         string_to_write = "Which wrist watches are Swiss wrist watches?"
         point_durability = self.pencil.point_durability
@@ -74,6 +77,22 @@ class TestPencilDurability(unittest.TestCase):
         self.pencil.write(self.paper, string_to_write)
 
         self.assertEqual(self.pencil.point_durability, point_durability - expected_score)
+
+    # Test to ensure that pencils can't write when their point durability is 0
+    def test_pencils_cannot_write_with_zero_durability(self):
+        self.pencil = Pencil(10)
+        number_of_characters = 20
+
+        # Fill the string with random characters
+        string_to_write = ""
+        for i in range(number_of_characters):
+            string_to_write += random.choice(string.ascii_lowercase)
+
+        self.pencil.write(self.paper, string_to_write)
+
+        paper_text_count = len(self.paper.text.strip())
+
+        self.assertEqual(paper_text_count, number_of_characters - self.pencil.start_point_durability)
 
 
 if __name__ == "__main__":
